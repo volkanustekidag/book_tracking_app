488521bf79b98f4d22bc675e302fb81ed403459d
volkanustekidag <volkanustekidg@gmail.com>
2022-06-16 11:20:48 +0300

    book list page app bar updated

--- lib/screens/book_list_page/book_list_page.dart
+++ lib/screens/book_list_page/book_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:book_tracking/blocs/book_bloc/book_bloc.dart';
+import 'package:book_tracking/components/app_bar.dart';
 import 'package:book_tracking/screens/book_details_page/book_details_page.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -17,22 +18,7 @@ class _BookListPageState extends State<BookListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: Icon(
-          Icons.menu,
-          color: Color(0xff2d2d2d),
-        ),
-        elevation: 0,
-        title: Text(
-          "BOOKTRACKİNG",
-          style: TextStyle(
-              color: Color(0xff2d2d2d),
-              fontWeight: FontWeight.w500,
-              fontSize: 18),
-        ),
-      ),
+      appBar: appbarBuild("BOOKTRACKİNG"),
       body: Container(
         color: Colors.white,
         padding: EdgeInsets.all(15),
