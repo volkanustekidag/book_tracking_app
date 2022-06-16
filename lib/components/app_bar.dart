faf8255ef46fa583362660212fc8f7087cd328ba
volkanustekidag <volkanustekidg@gmail.com>
2022-06-16 11:18:27 +0300

    app bar file createdd

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:book_tracking/const/constant.dart';
+import 'package:flutter/material.dart';
+
+AppBar appbarBuild(String title) {
+  return AppBar(
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    leading: Icon(
+      Icons.menu,
+      color: primaryColor,
+    ),
+    elevation: 0,
+    title: Text(
+      title,
+      style: TextStyle(
+          color: primaryColor, fontWeight: FontWeight.w500, fontSize: 18),
+    ),
+  );
+}
