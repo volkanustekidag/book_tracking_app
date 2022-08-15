f5c7e85eaa8ec324cb59087131aa481e79273d66
volkanustekidag <volkanustekidg@gmail.com>
2022-08-15 23:57:40 +0300

    Added visibility in Search TextFormField with Bloc

--- lib/screens/book_list_page/book_list_page.dart
+++ lib/screens/book_list_page/book_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:book_tracking/blocs/book_bloc/book_bloc.dart';
+import 'package:book_tracking/blocs/book_search_bloc/book_search_bloc.dart';
 import 'package:book_tracking/components/app_bar.dart';
 import 'package:book_tracking/screens/book_details_page/book_details_page.dart';
 import 'package:flutter/gestures.dart';
@@ -14,6 +15,25 @@ class BookListPage extends StatefulWidget {
 
 class _BookListPageState extends State<BookListPage> {
   final TextEditingController textEditingController = TextEditingController();
+  final ScrollController scrollController = new ScrollController();
+  @override
+  void initState() {
+    super.initState();
+
+    BlocProvider.of<BookSearchBloc>(context)
+        .add(ChangeSearchVisibilityEvent(visibility: true));
+
+    scrollController.addListener(() {
+      print(scrollController.position.pixels);
+      if (scrollController.position.pixels > 0) {
+        BlocProvider.of<BookSearchBloc>(context)
+            .add(ChangeSearchVisibilityEvent(visibility: false));
+      } else {
+        BlocProvider.of<BookSearchBloc>(context)
+            .add(ChangeSearchVisibilityEvent(visibility: true));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,44 +45,59 @@ class _BookListPageState extends State<BookListPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Color(0xffF6F8FA),
-                    borderRadius: BorderRadius.circular(10)),
-                child: TextFormField(
-                  controller: textEditingController,
-                  onChanged: (query) {
-                    BlocProvider.of<BookBloc>(context)
-                        .add(BookSearch(query: query));
-                    print(textEditingController.text);
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Color(0xffD0D0D2),
-                    ),
-                    suffixIcon: IconButton(
-                      splashColor: Colors.transparent,
-                      onPressed: () {
-                        WidgetsBinding.instance.addPostFrameCallback((_) {
-                          if (textEditingController.text.isNotEmpty) {
+            BlocConsumer<BookSearchBloc, BookSearchState>(
+              listener: (context, state) {},
+              builder: (context, state) {
+                if (state is BookVisibilityState) {
+                  return AnimatedOpacity(
+                    duration: Duration(seconds: 1),
+                    opacity: state.visibility ? 1 : 0,
+                    child: Visibility(
+                      visible: state.visibility,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Color(0xffF6F8FA),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: TextFormField(
+                          controller: textEditingController,
+                          onChanged: (query) {
                             BlocProvider.of<BookBloc>(context)
-                                .add(BookSearch(query: ""));
-                          }
-                          textEditingController.clear();
-                          FocusScope.of(context).unfocus();
-                        });
-                      },
-                      icon: Icon(Icons.close),
+                                .add(BookSearch(query: query));
+                            print(textEditingController.text);
+                          },
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Color(0xffD0D0D2),
+                            ),
+                            suffixIcon: IconButton(
+                              splashColor: Colors.transparent,
+                              onPressed: () {
+                                WidgetsBinding.instance
+                                    .addPostFrameCallback((_) {
+                                  if (textEditingController.text.isNotEmpty) {
+                                    BlocProvider.of<BookBloc>(context)
+                                        .add(BookSearch(query: ""));
+                                  }
+                                  textEditingController.clear();
+                                  FocusScope.of(context).unfocus();
+                                });
+                              },
+                              icon: Icon(Icons.close),
+                            ),
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                                color: Color(0xffD0D0D2),
+                                fontWeight: FontWeight.w500),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
                     ),
-                    hintText: "Search",
-                    hintStyle: TextStyle(
-                        color: Color(0xffD0D0D2), fontWeight: FontWeight.w500),
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
+                  );
+                }
+                return Container();
+              },
             ),
             Expanded(
               flex: 9,
@@ -82,7 +117,7 @@ class _BookListPageState extends State<BookListPage> {
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
                             Padding(
-                              padding: const EdgeInsets.only(bottom: 8),
+                              padding: const EdgeInsets.all(8.0),
                               child: Text(
                                 "All books(${state.books.length})",
                                 style: TextStyle(fontWeight: FontWeight.bold),
@@ -91,6 +126,7 @@ class _BookListPageState extends State<BookListPage> {
                             Expanded(
                               child: GridView.count(
                                 crossAxisCount: 2,
+                                controller: scrollController,
                                 childAspectRatio: 0.5,
                                 children: List.generate(books.length, (index) {
                                   return GestureDetector(
